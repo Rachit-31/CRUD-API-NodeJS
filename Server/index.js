638d11adadf470b361f166f00033ffb1359fe9ef
Rachit Jain <rachitjainjaipur@gmail.com>
2024-06-23 07:45:59 +0530

    .

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,20 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
+import dotenv from 'dotenv';
 
-const app=express();
-const PORT = 5000;
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+
+app.get( "/", (req, res)=>{
+    res.send("hello")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
